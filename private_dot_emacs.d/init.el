30216d6432805f7c586870a2020c50b953485dab
Sergei Antipov <greendayonfire@gmail.com>
2021-12-27 19:51:12 -0500

    Add more ignore line + update init.el

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -452,3 +452,16 @@ If the new path's directories does not exist, create them."
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-nerd-commenter company-box company lsp-ivy lsp-treemacs lsp-ui lsp-mode which-key visual-fill-column use-package rainbow-delimiters org-bullets ivy-rich hydra helpful general forge evil-collection doom-themes doom-modeline counsel-projectile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
